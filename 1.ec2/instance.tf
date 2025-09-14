10e5f3079c9a43ebbbf161ffd3db8bf7948f3f80
jagdevops19 <jsinat2016@gmail.com>
2025-09-14 07:03:13 +0100

    1.ec2 in default vpc-cidr block

--- 1.ec2/instance.tf
+++ 1.ec2/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/16"   #take cider block from default vpc for this workshop)
     #cidr_block = "172.31.0.0/16"
 }
 resource "aws_subnet" "main" {
